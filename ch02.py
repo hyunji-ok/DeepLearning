e833242b961cc3804a6f7d4cc6c0d22dca4fdf3b
hyunji-ok <hyunjiok10@gmail.com>
2020-02-24 12:17:31 +0900

    2장 코드

---
+++ ch02.py
@@ -0,0 +1,16 @@
+#2 퍼셉트론
+#2.3 퍼셉트론 구현하기
+#2.3.1 간단한 구현부터
+
+def AND(x1, x2):
+    w1, w2, theta = 0.5, 0.5, 0.7
+    tmp = x1*w1 + x2*w2
+    if tmp <= theta:
+        return 0
+    elif tmp > theta:
+        return 1
+
+print(AND(0, 0))
+print(AND(0, 1))
+print(AND(1, 0))
+print(AND(1, 1))
